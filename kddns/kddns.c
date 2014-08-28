7d41cc3e773fba992a4e9d51d30cd58cd5cb0a1a
millken <millken@gmail.com>
2014-08-28 15:56:04 +0800

    init from https://github.com/ritsz/kernel_modules_hacking/blob/master/packet_hello.c

--- kddns/kddns.c
+++ kddns/kddns.c
@@ -13,14 +13,40 @@
 #define KFDNS_PROCFS_STAT "kfdns"
 #define DNS_HEADER_SIZE 12
 
-static int kddns_init(void)
+static struct nf_hook_ops nfho; //net filter hook option struct
+
+
+unsigned int kddns_packet_hook(unsigned int hooknum,
+						struct sk_buff *skb,
+						const struct net_device *in,
+						const struct net_device *out,
+						int (*okfn)(struct sk_buff *))
+	{
+	struct sock *sk = skb->sk;
+	printk("Hello packet!");
+	return NF_ACCEPT;
+}
+
+static int init_filter_if(void)
 {
-	printk(KERN_INFO "Starting kddns module\n");
+	nfho.hook = kddns_packet_hook;
+	nfho.hooknum = 0 ; //NF_IP_PRE_ROUTING;
+	nfho.pf = PF_INET;
+	nfho.priority = NF_IP_PRI_FIRST;
+	nf_register_hook(&nfho);
 	return 0;
 }
 
-static void kddns_exit(void)
+static int __init kddns_init(void)
 {
+	printk(KERN_INFO "Starting kddns module\n");
+	init_filter_if();
+	return 0; // Non-zero return means that the module couldn't be loaded.
+}
+
+static void __exit kddns_exit(void)
+{
+	nf_unregister_hook(&nfho);
 	printk(KERN_INFO "Stoping kddns module\n");
 }
 
